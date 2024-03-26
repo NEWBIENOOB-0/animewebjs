d83e8b72fd14eb1ecec0fa929f8af7e5e09bd2cf
NEWBIEN00B <jidnyeshspatil@gmail.com>
2024-03-26 10:31:53 +0530

    my own api update

--- js/episode.js
+++ js/episode.js
@@ -7,7 +7,7 @@ const dlapi = "/download/";
 
 // Api Server Manager
 
-const AvailableServers = ["https://api13.anime-dex.workers.dev"];
+const AvailableServers = ["https://api.cloudyflare.workers.dev"];
 
 function getApiServer() {
     return AvailableServers[Math.floor(Math.random() * AvailableServers.length)];
